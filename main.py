e984dadc6db64f7a757ffff28b7e1bfd563fa70c
Nicolas Boiteau <nicolas.boiteau@efrei.net>
2023-03-26 00:54:35 +0100

    update25

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ def showtab(f):
             tempo = 1
         if temp[1] == "c":
             tempo = 2
+        if temp[1] == "d":
+            tempo = 3
+        if temp[1] == "e":
+            tempo = 4
         if tab[int(temp[0])][tempo][carrier]!= " ":
             carrier = 1
             deterministic = False
@@ -64,6 +68,8 @@ def menu():
     file_open = file+str(m)+".txt"
     print(file_open,"\n\n")
     f = open(file_open,'r')
+    if m == 1:
+        f= open("automate_1", 'r')
     return f,m
 
 
@@ -111,10 +117,8 @@ def print_tab(f,tab,deterministic):
 
     for k in range(2, 2 * nb_term + 1, 2):
         set_term.append(term[k])
-    if deterministic:
-        print("        |____a____|____b____|")
-    else:
-        print("        |_____a_____|_____b_____|")
+    for i in range(97, 97 + a):
+        print("_____", chr(i), "_____|", end="")
     for i in range(0,p):
         if i<nb_term:
             vf = int(set_term[i])
@@ -140,6 +144,9 @@ def print_tab(f,tab,deterministic):
     return tab
 
 def standardisation(f,tab,standard,m):
+    if standard:
+        print("the automaton is alredy standard\n")
+        return tab
     f.seek(0)
     a = int(f.readline())
     p = int(f.readline())
@@ -154,11 +161,16 @@ def standardisation(f,tab,standard,m):
     counterx = 0
     countery = 0
     nb_transi = int(f.readline())
-    new_Init_states = [[kar for i in range(3)] for j in range(nb_transi)]
+    new_Init_states = [[kar for i in range(a)] for j in range(nb_transi)]
     new_inputx = []
 
     new_inputy = []
-
+    new_inputc = []
+    new_inputd = []
+    count_a = 0
+    count_b = 0
+    count_c = 0
+    count_d = 0
     for k in range(2, 2 * nb_init + 1, 2):
         set_init.append(init[k])
 
@@ -178,14 +190,42 @@ def standardisation(f,tab,standard,m):
             print(stringer)
             if transi[1] =='a':
                 new_inputx.insert(0,int(transi[2]))
-                countery += 1
+                count_a +=1
+
             if transi[1] == 'b':
                 new_inputy.insert(0,int(transi[2]))
+            if transi[1] =='c':
+                new_inputc.insert(0,int(transi[2]))
+            if transi[1] == 'd':
+                new_inputd.insert(0,int(transi[2]))
 
 
-    last_inset=[new_inputx,new_inputy]
+
+    count_a = len(new_inputx)
+    count_b = len(new_inputy)
+    count_c = len(new_inputc)
+    count_d = len(new_inputd)
+    while count_a<a:
+        new_inputx.insert(0, " ")
+        count_a +=1
+    while count_b < a:
+        new_inputy.insert(0, " ")
+        count_b += 1
+    while count_c<a:
+        new_inputc.insert(0, " ")
+        count_c +=1
+    while count_d < a:
+        new_inputd.insert(0, " ")
+        count_d += 1
+
+    if a ==2:
+        last_inset=[new_inputx,new_inputy]
+    if a ==3:
+        last_inset=[new_inputx,new_inputy,new_inputc]
+    if a ==4:
+        last_inset=[new_inputx,new_inputy,new_inputc,new_inputd]
     tab.insert(0, last_inset)
-    pprint.pprint(tab)
+
     return tab
 
 
@@ -194,6 +234,7 @@ def standardisation(f,tab,standard,m):
 
         # create transition from I to the transition of past Initial states
 def print_tab_from_tab(f,tab,standardised):
+
     f.seek(0)
     a = f.readline()
     q = f.readline()
@@ -207,45 +248,69 @@ def print_tab_from_tab(f,tab,standardised):
     set_init = []
     set_term = []
     counter = 0
+
+    kar = " "
     x = 0
+    b = len(tab[0][0])
+
     for k in range(2, 2 * nb_init + 1, 2):
         set_init.append(init[k])
 
     for k in range(2, 2 * nb_term + 1, 2):
         set_term.append(term[k])
-    print("      ",end="")
+    print(set_term,"\n",set_init,"\n\n\n")
+    print("       ",end="")
+
     for i in range(97,97+a):
 
         print("_____",chr(i),"_____|",end="")
     print(" ")
-    for i in range(0,q+1):
-        if i<nb_term:
-            vf = int(set_term[i])
-        if i<nb_init:
-            vb = int(set_init[i])
+    if standardised:
+        q=q+1
+    conteur_entrant = 0
+    conteur_sortant = 0
+    for i in range(0,q):
+        if conteur_sortant<nb_term:
+            vf = int(set_term[conteur_sortant])
+
+        if conteur_entrant<nb_init:
+            vb = int(set_init[conteur_entrant])
+
         if vf ==i and vb!=i:
             print("<- ", end="")
+            conteur_sortant +=1
+
 
         if vb == i and vf:
             print("-> ",end="")
+            conteur_entrant +=1
         if vb!=i and vf!=i:
             print("   ",end="")
         if vb == i == vf:
             print("<->",end="")
+            conteur_sortant+=1
+            conteur_entrant +=1
         if standardised:
             if i==0:
-                print("I",end="   | ")
+                print("->  I",end="   | ")
             else:
                 print(i-1,end="   | ")
+        else:
+            print(i, end="   | ")
         for k in range(0,a):
             print(end="   ")
-            for r in range(0,a):
-                if tab[i][k][r]== " ":
-                    print("",end="")
-                else:
+            counter = a
+            for v in range(0, b):
+                if tab[i][k][v]== ' ':
+                    counter = counter -1
+
+            for r in range(0,b-1):
+                if counter>1:
                     print(tab[i][k][r],end=",")
-                if r ==a-1 :
-                    print(end="    |")
+                else:
+                    print(tab[i][k][r], end="")
+            print(tab[i][k][b-1],end="    |")
+
             print(end="   ")
         print(" ")
 
@@ -253,22 +318,50 @@ def print_tab_from_tab(f,tab,standardised):
 
 
 
-
-
+def sous_menu():
+    running = True
+    print(" Hi, Welcome to our finite automata's Project")
+    print(" First of all chose the automaton you want : \n\n")
+    f = menu()
+    choice = -1
+    standardized = False
+    while(running):
+        print("1) to change to automaton u choosed \n2) to show the information of the automaton\n3) to standardize this automaton\n4) to determinize:\n5) EXIT")
+        choice = int(input())
+        if choice == 1:
+            f = menu()
+            standardized = False
+        if choice == 2:
+            show_info(f[0])
+            x = showtab(f[0])
+            tab = x[0]
+            print_tab_from_tab(f[0], tab,standardized)
+        if choice == 3:
+            x = showtab(f[0])
+            tab = x[0]
+            print_tab_from_tab(f[0],tab,standardized)
+            standardisation(f[0], tab, x[2], f[1])
+            standardized = True
+        if choice == 5:
+            running = False
 
 
 def main():
+    sous_menu()
     f = menu()
 
     show_info(f[0])
     x =showtab(f[0])
     tab =print_tab(f[0],x[0],x[1])
+    stanardized = False
+    #print_tab_from_tab(f[0], tab, stanardized)
     if x[2] == False:
         standardisation(f[0],tab,x[2],f[1])
         stanardized = True
-        print_tab_from_tab(f[0],tab,stanardized)
+    print_tab_from_tab(f[0],tab,stanardized)
 
-    print(tabulate(tab, headers='a b c', tablefmt='fancy_grid'))
+    print(tabulate(tab, headers='abcd', tablefmt='fancy_grid'))
+    #tranform_tab(f,tab)
     return 0
 
 main()
